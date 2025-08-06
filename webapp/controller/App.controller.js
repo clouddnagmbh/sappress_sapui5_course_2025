ca0ce156ada770828bbf688839920e4d014f1298
Daniel Krancz <44464423+danielkrancz@users.noreply.github.com>
2025-08-06 20:20:36 +0200

    Initial commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("com.sappress.customerapp.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comsappress/customerapp/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
